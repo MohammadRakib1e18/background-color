b38814a56cb59972f8501de6665ba1f2bca2d7cf
MohammadRakib1e18 <trustedrakib@gmail.com>
2021-09-05 12:19:36 +0600

    added random color options

--- js/app.js
+++ js/app.js
@@ -1,14 +1,50 @@
-const setColor = color => {
-     document.getElementById('body').style.backgroundColor =color;
+
+let choice = [
+    [255,0,0], [0,128,0], [0,255,255], [0,0,255], [255,255,0]
+]
+
+let pickerColor = () => {
+    console.log(choice[0]);
+    document.getElementById('red').style.backgroundColor   = `rgb(${choice[0][0]},${choice[0][1]},${choice[0][2]})`;
+    document.getElementById('green').style.backgroundColor = `rgb(${choice[1][0]},${choice[1][1]},${choice[1][2]})`;
+    document.getElementById('cyan').style.backgroundColor  = `rgb(${choice[2][0]},${choice[2][1]},${choice[2][2]})`;
+    document.getElementById('blue').style.backgroundColor  = `rgb(${choice[3][0]},${choice[3][1]},${choice[3][2]})`;
+    document.getElementById('yellow').style.backgroundColor= `rgb(${choice[4][0]},${choice[4][1]},${choice[4][2]}`;
 }
+const setColor = color => {
+     document.getElementById('body').style.backgroundColor =`rgb(${color[0]},${color[1]},${color[2]})`;
+}
+setColor([136, 171, 0]);
 
 document.getElementById('buttons').addEventListener('click', function(event){
     const getIdColorName = event.target.id;
-    setColor(getIdColorName);
+
+    switch (getIdColorName) {
+        case 'red': setColor(choice[0]);
+            break;
+        case 'green': setColor(choice[1]);
+            break;
+        case 'cyan': setColor(choice[2]);
+            break;
+        case 'blue': setColor(choice[3]);
+            break;
+        case 'yellow': setColor(choice[4]);
+    }
 })
 
+// set initial/default color(white) 
 document.getElementById('default').addEventListener('click', function(){
     document.getElementById('body').style.setProperty('background-color', 'initial');
 })
 
-setColor('orange');
+
+
+// generate new color set
+document.getElementById('new-colorSet').addEventListener('click', function(){
+    for(let i=0; i<5; i++){
+        choice[i][0]=parseInt(Math.random()*255);
+        choice[i][1]=parseInt(Math.random()*255);
+        choice[i][2]=parseInt(Math.random()*255);
+    }
+    pickerColor();
+})
